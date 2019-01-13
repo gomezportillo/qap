ef08929bc675ef7a78b8ac15d39eabc478d6b5a6
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-01-13 22:12:04 +0100

    turned out that the simplifation was not correct. trying to fix it

--- src/baldwinian.py
+++ src/baldwinian.py
@@ -25,7 +25,10 @@ class Baldwinian(GeneticAlgorithm):
 
 
     def calculate_fitness(self, generation):
+        counter=0
         for individual in generation:
+            counter += 1
+            print("individual ", counter)
             optimized_individual = self.greedy_optimization( individual )
             individual.fitness = optimized_individual.fitness
 
@@ -34,46 +37,38 @@ class Baldwinian(GeneticAlgorithm):
         """
         Greedy 2-opt algorithm implementing the pseudocode given on the
         problem statement.
+        The do/while loop has been ommited from the algorithm as it is virtually
+        impossible that after all the permutations S == best, and even though if
+        that was the case it would not change for executing for loops again.
         """
-        size = individual.size
         S = deepcopy( individual )
         super().calculate_individual_fitness( S )
-        # print("greedy")
 
-        result_fount = False
-        # counter = 1
-        while not result_fount:
-            best = deepcopy( S )
-            # print("pre-for ", counter)
+        best = deepcopy( S )
 
-            for i in range(size):
-                # print("post-for {}/{}".format(i+1, size))
+        for i in range( S.size ):
+            for j in range(i + 1, S.size):
+                T = deepcopy( S )
+                T.chromosomes[i] = S.chromosomes[j]
+                T.chromosomes[j] = S.chromosomes[i]
 
-                for j in range(i + 1, size):
-                    T = deepcopy( S )
-                    T.chromosomes[i] = S.chromosomes[j]
-                    T.chromosomes[j] = S.chromosomes[i]
+                # super().calculate_individual_fitness( T )
+                self.calculate_fitness_after_swap( T, i, j )
 
-                    self.calculate_fitness_after_swap( S, T, i, j )
+                if T < S:
+                    S = deepcopy( T )
 
-                    if T < S:
-                        S = deepcopy( T )
-
-            if S < best:
-                result_fount = True
-            # counter+= 1
         return S
 
 
-    def calculate_fitness_after_swap(self, S, T, i, j):
+    def calculate_fitness_after_swap(self, T, val1, val2):
         """
         Instead of calculating again the whole fitness, it is only a matter of
-        calculing the chromosomes that have been changed, decreasing the
-        complexity of the function from quadratic to lineal
-        Fitness(T) = Fitness(S) - Fitness(S)(ij) + Fitness(F)(ij)
+        calculing the chromosomes that have been changed.
+        Complexity reduced from n^2 to 2n, being n the problem_size
         """
-        fitnessSij = self.flow_matrix[i][j] * self.distance_matrix[S.chromosomes[i]][S.chromosomes[j]]
-        fitnessTij = self.flow_matrix[i][j] * self.distance_matrix[T.chromosomes[i]][T.chromosomes[j]]
+        counter = 0
+        for i in range(self.problem_size):
+            counter += 1
 
-        new_fitness = S.fitness - fitnessSij + fitnessTij
-        T.fitness = new_fitness
+        # T.fitness = new_fitness
